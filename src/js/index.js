283178f09d7c661b1b51b03201d5181c188b9464
Iron318i <iron318i@gmail.com>
2020-03-12 16:13:22 +0300

    16-10

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
 import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+import 'owl.carousel';
+
 
 +function ($) {
-
+    $('.nav-events').owlCarousel({
+	margin: 0,
+	loop: false,
+	dots: false,
+	nav: true,
+	autoWidth: true,
+	items: 4
+    });
 }(jQuery);
