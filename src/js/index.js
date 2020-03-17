ecf1e2721380ab2b981f325d41433988d730bb2f
Iron318i <iron318i@gmail.com>
2020-03-17 12:38:43 +0300

    12-30

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,7 @@ import 'owl.carousel';
     $('.block-header-menu .owl-carousel').owlCarousel({
 	margin: 15,
 	loop: false,
-	dots: true,
+	dots: false,
 	nav: true,
 	navText: [
 	    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="currentColor" d="M8.1 10l7.5-7.5c.2-.2.3-.5.3-.8 0-.3-.1-.6-.3-.8l-.7-.7c-.2-.1-.5-.2-.8-.2-.3 0-.6.1-.8.3L4.4 9.2c-.2.2-.3.5-.3.8 0 .3.1.6.3.8l8.9 8.9c.2.2.5.3.8.3.3 0 .6-.1.8-.3l.7-.7c.4-.4.4-1.1 0-1.5L8.1 10z"></path></svg>',
